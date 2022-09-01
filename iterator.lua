3fa85e6f2e88e97a89ffa929eaa7b50a2eb30967
iTeller_zc(chao.zhang09) <chao.zhang09@weimob.com>
2022-09-01 16:53:17 +0800

    添加迭代器和表 基础示例

--- iterator.lua
+++ iterator.lua
@@ -3,8 +3,8 @@ for key,value in ipairs(array) do
 	print(key,value)
 end
 
-array = {nil,'zc'}
-for key,value in ipairs(array) do
+array2 = {nil,'zc'}
+for key,value in ipairs(array2) do
 	print(key,value)
 end
 
@@ -26,3 +26,33 @@ do
 	print(i,n)
 end
 
+--自定义 ipairs实现
+
+function iter (a, i)
+    i = i + 1
+    local v = a[i]
+    if v then
+       return i, v
+    end
+end
+
+function ipairs (a)
+    return iter, a, 0
+end
+
+
+--多状态迭代器
+function elementIterator(collection)
+	local index = 0
+	count=#collection
+	return function()
+		index = index +1
+		if index <= count then
+			return collection[index]
+		end
+	end
+end
+
+for element in elementIterator(array) do
+	print(element)
+end
--- table.lua
+++ table.lua
@@ -26,3 +26,23 @@ for i=1,10 do
  a3[i] = i
  print(a3[i])
 end
+
+print(type(tab1))
+
+
+--table  函数
+--concat(table,sep,start,end)
+--insert(table,pos,value)
+--maxn(table) 5.2后版本已移除该函数
+--remove(table,pos)
+--sort(table,comparator)
+
+--当我们获取 table 的长度的时候无论是使用 # 还是 table.getn 其都会在索引中断的地方停止计数，而导致无法正确取得 table 的长度。替代方案
+
+function table_leng(t)
+  local leng=0
+  for k, v in pairs(t) do
+    leng=leng+1
+  end
+  return leng;
+end
